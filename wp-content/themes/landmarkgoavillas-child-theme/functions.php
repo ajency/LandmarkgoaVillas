86473f0b1e496fce022e6d3579e39e2abb21cb2a
Ankita <ankita@ajency.in>
2019-09-09 11:41:14 +0530

    updated files

--- wp-content/themes/landmarkgoavillas-child-theme/functions.php
+++ wp-content/themes/landmarkgoavillas-child-theme/functions.php
@@ -17,3 +17,34 @@ if (! function_exists('pado_child_styles')) {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'pado_child_styles');
+
+function footer_widgets_init() {
+	register_sidebar( array(
+		'name' => 'Footer Sidebar 1',
+		'id' => 'footer-sidebar-1',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	register_sidebar( array(
+		'name' => 'Footer Sidebar 2',
+		'id' => 'footer-sidebar-2',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	register_sidebar( array(
+		'name' => 'Footer Sidebar 3',
+		'id' => 'footer-sidebar-3',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+add_action('widgets_init','footer_widgets_init');
